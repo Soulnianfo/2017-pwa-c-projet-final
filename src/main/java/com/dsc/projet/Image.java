6e0c3ad283d74340d9155c79136b8fd96738e329
MNour7 <mahamadou.nour7@gmail.com>
2017-10-30 22:03:35 +0100

    definition of Image class

---
+++ src/main/java/com/dsc/projet/Image.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dsc.projet;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.Data;
+
+/**
+ *
+ * @author Noura
+ */
+@Entity
+@Data
+public class Image {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    long id;
+    
+    String url;
+    
+    String alt;
+
+    public Image() {
+        
+    }
+   
+    public Image(String url, String alt) {
+        this.url = url;
+        this.alt = alt;
+    }
+    
+    
+}
